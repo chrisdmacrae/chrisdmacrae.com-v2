15b282ecf284920ad7fe5716ade2e4e78f3dd06e
Chris D. MacRae <hello@chrisdmacrae.com>
2020-08-22 00:57:27 -0300

    Add IS_PRODDUCTION env var

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   env: {
+    IS_PRODUCTION: process.env.NODE_ENV === "production",
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     REPO_FULL_NAME: process.env.REPO_FULL_NAME,
     BASE_BRANCH: process.env.BASE_BRANCH,
