fae43f1ecf450871ac2b420b874d723b98bc0fcb
Chris D. MacRae <hello@chrisdmacrae.com>
2020-08-24 00:39:41 -0300

    Update to handle basic dark mode

--- src/lib/core/components/Navigation/NavBar.tsx
+++ src/lib/core/components/Navigation/NavBar.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Container, Nav, Navbar, Row } from 'react-bootstrap';
+import { useColorScheme } from '../../hooks/useColorScheme';
 
 export interface NavBarProps {}
 
 export function NavBar() {
+  let [scheme] = useColorScheme("light");
+  
   return (
-    <Navbar expand="lg">
+    <Navbar expand="lg" variant={scheme === "light" ? "light" : "dark"}>
       <Container>
-        <Navbar.Brand href="/">Chris D. Macrae</Navbar.Brand>
+        <Navbar.Brand href="/" className="mr-auto">Chris D. Macrae</Navbar.Brand>
         <Navbar.Toggle aria-controls="primary-nav" />
         <Navbar.Collapse id="primary-nav">
-          <Nav>
-            <Nav.Link href="/">Home</Nav.Link>
+          <Nav as="ul" className="mr-auto">
+            <Nav.Item as="li">
+              <Nav.Link href="/">Home</Nav.Link>
+            </Nav.Item>
           </Nav>
-          </Navbar.Collapse>
-        </Container>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
-  )
+  );
 }
