982714cd3f4f6486bb3f8445c6ed48b76f2e1542
Chris D. MacRae <hello@chrisdmacrae.com>
2020-08-27 13:27:14 -0300

    Add a main tag wrapper for styling only the actual site

--- src/lib/core/layouts/Base.tsx
+++ src/lib/core/layouts/Base.tsx
@@ -16,7 +16,9 @@ export default function BaseLayout({ page, children }: BaseLayoutProps) {
         <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700|Source+Sans+Pro:400,600,700" rel="stylesheet" />
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossOrigin="anonymous" />
       </Head>
-      {children}
+      <main>
+        {children}
+      </main>
     </>
   )
 }
