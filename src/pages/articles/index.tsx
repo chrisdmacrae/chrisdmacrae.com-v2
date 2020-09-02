9d86e1028f27942bd7b189ea7dfcabb809320281
Chris D. MacRae <hello@chrisdmacrae.com>
2020-09-02 11:53:11 -0300

    Update articles to fetch from remote in preview mode

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -12,7 +12,7 @@ export const getStaticProps: GetStaticProps = async function ({
   preview,
   previewData,
 }) {
-  const articlePaths = await getAllArticlePaths();
+  const articlePaths = await getAllArticlePaths(preview);
   let props = {
     isEditing: preview ?? false,
     page: {},
